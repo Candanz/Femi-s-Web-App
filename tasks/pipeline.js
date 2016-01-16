62b490ff7d27a21355243fa4b84b5df74c3aadba
Candanz <justcandan@gmail.com>
2016-01-16 02:25:27 +0100

    Bootstrap included into project
    Added bootstrap dependencies and setup correct orders.
    Had to change font location in bootstrap css to load them from external
    font folder instead of having it live in the styles folder.

--- tasks/pipeline.js
+++ tasks/pipeline.js
@@ -28,6 +28,7 @@ var jsFilesToInject = [
   'js/dependencies/sails.io.js',
 
   // Dependencies like jQuery, or Angular are brought in here
+  'js/dependencies/jquery/jquery.js',
   'js/dependencies/**/*.js',
 
   // All of the rest of your client-side js files
